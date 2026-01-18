b0b5291e0fc353d43ec44d9ca93416f9904f3811
Gaurav Yadav <gauravyadav@Saurabhs-MacBook-Air.local>
2026-01-18 06:54:01 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import { fileURLToPath } from "url";
+import path from "path";
+
+
+
+
 import authRouters from "./routes/auth.route.js";
 import messagesRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const app = express();
+//deplyoment code
+const ___dirname = path.resolve();
 
 const PORT = process.env.PORT||3000;
 
 app.use("/api/auth" ,  authRouters);
 app.use("/api/messages" , messagesRoutes);
+
+//make ready for deployment
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(___dirname,"../frontend/dist")))
+  app.get("*" , (__,res)=>{
+    res.sendFile(path.join(___dirname , "../frontend" , "dist" , "index.html"));
+  })
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
